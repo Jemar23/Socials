8f8dbd0d4b18121f07e08d5398d0004814a48317
Jemar23 <jemarjohnson116@icloud.com>
2022-03-25 16:52:15 -0400

    Added first border

--- link-tree/src/App.js
+++ link-tree/src/App.js
@@ -3,18 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>New App.</p>
       <Name />
+      <InfoBox />
     </div>
   );
 }
 
 function Name() {
    return (
-     <div>
+     <div id="first" className="firstName">
        <h1>Jemar Johnson</h1>
+       <p>Student Software Engineer</p>
      </div>
-   )
+   );
+}
+
+function InfoBox() {
+  return (
+    <div className="infoBox">
+      <p>OpenSea NFT portfolio</p>
+    </div>
+  )
 }
 
 export default App;
