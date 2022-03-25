0cd247563628c76dde17c51c6bb60b84002c6f9f
Jemar23 <jemarjohnson116@icloud.com>
2022-03-25 19:16:40 -0400

    Added tailwind css

--- link-tree/src/App.js
+++ link-tree/src/App.js
@@ -11,17 +11,19 @@ function App() {
 
 function Name() {
    return (
-     <div id="first" className="firstName">
-       <h1>Jemar Johnson</h1>
-       <p>Student Software Engineer</p>
+     <div className="flex flex-col items-center">
+       <h1 className="text-2xl font-extrabold">Jemar Johnson</h1>
+       <p className="text-lg font-bold">Student Software Engineer</p>
      </div>
    );
 }
 
 function InfoBox() {
   return (
-    <div className="infoBox">
-      <p>OpenSea NFT portfolio</p>
+    <div className="w-full space-y-4 pt-2 row-3">
+      <p className="text-center text-lg">OpenSea NFT portfolio</p>
+      <p className="text-center text-lg">Monitors</p>
+      <p className="text-center text-lg">Camera</p>
     </div>
   )
 }
