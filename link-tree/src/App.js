6fb92989578d5b92cda9922fd481921ae3dcf218
Jemar23 <jemarjohnson116@icloud.com>
2022-03-24 20:29:13 -0400

    Added my full name and changed background color to gray

---
+++ link-tree/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>New App.</p>
+      <Name />
+    </div>
+  );
+}
+
+function Name() {
+   return (
+     <div>
+       <h1>Jemar Johnson</h1>
+     </div>
+   )
+}
+
+export default App;
